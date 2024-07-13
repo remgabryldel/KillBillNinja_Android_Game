baad1e3cf6070269c50eca3260aee1fc3972e206
remgabryldel <rem.gabryldellarosa@stud.uniroma3.it>
2024-07-13 13:29:33 +0200

    Spawner e Blade
    implementazione dell'oggetto Spawner, che genera la frutta con un impulso di fronte alla camera di gioco, implementato il codice e verificata la correttezza dello script in esecuzione.
    Inizializzazione dell'implementazione di Blade e creazione del relativo script.

---
+++ Fruit Ninja/Assets/Scripts/Blade.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private void Update(){
+        
+    }
+
+}
---
+++ Fruit Ninja/Assets/Scripts/Spawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] fruitPrefabs;
+
+    public float maxTempovita = 5f;
+
+    public float minSpawnDelay = 0.15f;
+    public float maxSpawnDelay = 1.5f;
+
+    public float minForza = 18f;    
+    public float maxForza = 22f;
+
+    public float minAngolo = -25f;
+    public float maxAngolo = 25f;
+
+    private Collider spawnArea;
+
+    private void Awake()
+    {
+        spawnArea = GetComponent<Collider>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator Spawn(){
+
+        yield return new WaitForSeconds(2f);
+
+        while (enabled){
+            GameObject prefab = fruitPrefabs[Random.Range(0, fruitPrefabs.Length)];
+
+            Vector3 posizione = new Vector3();
+            posizione.x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x);
+            posizione.y = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y);
+            posizione.z = Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z);
+
+            Quaternion rotazione = Quaternion.Euler(0f, 0f, Random.Range(minAngolo, maxAngolo));
+
+            GameObject frutto = Instantiate(prefab, posizione, rotazione);
+            Destroy(frutto, maxTempovita);
+
+            float forza = Random.Range(minForza, maxForza);
+            frutto.GetComponent<Rigidbody>().AddForce(frutto.transform.up * forza, ForceMode.Impulse);
+
+
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+        }
+
+    }
+
+}
